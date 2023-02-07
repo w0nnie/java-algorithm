e714efaa7a5c9800e1521be81c89224a24ac1e56
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-02-07 13:25:35 +0900

    Update 구간 합 구하기 4.java

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,19 +8,45 @@ public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int suNo = Integer.parseInt(st.nextToken()); // 데이터의 개수
         int quNo = Integer.parseInt(st.nextToken()); // 퀴즈 개수
-        long[] s = new long[suNo + 1];
-        st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=suNo; i++){
-            s[i] = s[i-1] + Integer.parseInt(st.nextToken());
-        }
-        for(int q=0; q<quNo; q++){
+        int[] sumArr = new int[suNo + 1]; //구간합배열 1번인덱스 부터
+        
+        sumArrAdd(br, suNo, sumArr);
+        solution(br, quNo, sumArr);
+    }
+
+    /**
+     * start, end 값 입력과 동시에  
+     * sumArr배열에 구간합 공식을 적용하여 원하는값을 도출
+     * @param br
+     * @param quNo
+     * @param sumArr
+     * @throws IOException
+     */
+    private static void solution(BufferedReader br, int quNo, int[] sumArr) throws IOException {
+        StringTokenizer st;
+        for(int q = 0; q< quNo; q++){
             st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            System.out.println(s[j] - s[i-1]);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(sumArr[end] - sumArr[start-1]);
         }
     }
 
+    /**
+     * sumArr 누적
+     * @param br
+     * @param suNo
+     * @param sumArr
+     * @throws IOException
+     */
+    private static void sumArrAdd(BufferedReader br, int suNo, int[] sumArr) throws IOException {
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<= suNo; i++){
+            sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
+        }
+    }
 }
