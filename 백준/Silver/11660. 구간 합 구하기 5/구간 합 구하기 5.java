067bc3c2a64980fa4267515bac7888ba077641d4
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-02-12 21:17:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1684 ms, Memory: 129220 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -7,31 +7,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int arrSize = Integer.parseInt(st.nextToken());
-        int quiz = Integer.parseInt(st.nextToken());
-        long s[][] = new long[arrSize + 1][arrSize + 1];
-        for(int i=1; i<=arrSize; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=arrSize; j++){
-                s[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        long sum[][] = new long[arrSize + 1][arrSize + 1];
-        for(int i=1; i<=arrSize; i++){
-            for(int j=1; j<=arrSize; j++){
-                sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + s[i][j];
-            }
-        }
-        for(int i=0; i<quiz; i++){
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
 
-            long result = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
-            System.out.println(result);
+        int N = Integer.parseInt(st.nextToken());
+        int question = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N+1][N+1];
+        int[][] sumArr = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                //구간합 공식
+                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] -sumArr[i-1][j-1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < question; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()); //2
+            int y1 = Integer.parseInt(st.nextToken()); //2
+            int x2 = Integer.parseInt(st.nextToken()); //3
+            int y2 = Integer.parseInt(st.nextToken()); //4
+            System.out.println(sumArr[x2][y2] - sumArr[x1 - 1][y2] - sumArr[x2][y1 - 1] + sumArr[x1 - 1][y1 - 1]);
+
         }
     }
-
 }
