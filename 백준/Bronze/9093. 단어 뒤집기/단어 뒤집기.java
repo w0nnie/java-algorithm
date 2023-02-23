e7109b115500f189a3bc0df1a657496b08959fe3
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-02-23 22:39:51 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 608 ms, Memory: 58084 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < n; i++) {
+            Stack stack = new Stack();
+            String s = br.readLine();
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == ' ') {
+                    while(!stack.isEmpty()){
+                        bw.write(String.valueOf(stack.pop()));
+                    }
+                    bw.write(" ");
+                }else{
+                    stack.add(s.charAt(j));
+                }
+            }
+            while(!stack.isEmpty()){
+                bw.write(String.valueOf(stack.pop()));
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
