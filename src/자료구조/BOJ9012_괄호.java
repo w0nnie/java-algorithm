0d88715c423da1bb84098489451cfd7c587773c1
jinwon <kjw55523@gmail.com>
2023-02-25 00:46:25 +0900

    TIL

--- src/자료구조/BOJ9012_괄호.java
+++ src/자료구조/BOJ9012_괄호.java
@@ -1,34 +1,42 @@
 package 자료구조;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class BOJ9012_괄호 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt(); //6
-        for (int i = 0; i < num; i++) {
-            solve(sc.next());
+
+    static BufferedReader br;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        int rowCount = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < rowCount; i++) {
+            Stack<Character> stack = new Stack<>();
+            String vps = br.readLine();
+            solve(stack, vps);
         }
     }
-    public static void solve(String str){
-        Stack<Character> stack = new Stack<>();
-        for (int j = 0; j < str.length(); j++) {
-            if(str.charAt(j) == '('){
-                stack.push(str.charAt(j));
-            }else if(str.charAt(j) == ')'){
-                if(!stack.isEmpty()){
+
+    private static void solve(Stack<Character> stack, String vps) {
+        for (int j = 0; j < vps.length(); j++) {
+            if (vps.charAt(j) == '(') {
+                stack.push(vps.charAt(j));
+            } else if (vps.charAt(j) == ')') {
+                if (stack.isEmpty()) {
+                    stack.push(vps.charAt(j));
+                    break;
+                } else {
                     stack.pop();
-                }else{
-                    System.out.println("NO");
-                }
-            }else{
-                if(stack.isEmpty()){
-                    System.out.println("YES");
-                }else{
-                    System.out.println("NO");
                 }
             }
+        }
+        if (stack.isEmpty()) {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
         }
     }
 }
