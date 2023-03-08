29f06d90fd3832f95c33d58317c8010e8a153699
w0nnie <kjw55523@gmail.com>
2023-03-08 13:52:30 +0900

    BOJ1874_스택수열

---
+++ src/자료구조/BOJ1874_스택수열.java
@@ -0,0 +1,47 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ1874_스택수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            arr[i] = temp;
+        }
+        int num = 1;
+        boolean flag = true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int index = arr[i];
+            if (index >= num) {
+                while (index >= num) {
+                    stack.push(num);
+                    num++;
+                    sb.append("+ \n");
+                }
+                stack.pop();
+                sb.append("- \n");
+            }else{
+                int a = stack.pop();
+                if (a > index) {
+                    System.out.println("NO");
+                    flag = false;
+                    break;
+                }else{
+                    sb.append("- \n");
+                }
+            }
+        }
+        if(flag){
+            System.out.println(sb);
+        }
+    }
+}
+
