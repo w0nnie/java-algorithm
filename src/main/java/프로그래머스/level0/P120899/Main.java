f938da48fdf017d027c9d1d812f87505050f4bff
jinwon <kjw55523@gmail.com>
2023-03-18 20:25:50 +0900

    P120899

--- src/main/java/프로그래머스/level0/P120899/Main.java
+++ src/main/java/프로그래머스/level0/P120899/Main.java
@@ -11,6 +11,14 @@ public class Main {
     }
 }
 
+/**
+ * Integer 타입의 arrayList를 하나 만들어주고
+ * 인자값으로 들어온 array에 있는 원소값들을
+ * 다 add 해준 후
+ * Collections.max 메서드를 이용하여 list의 max값을 찾아줬다
+ * 해당 max값으로 max값이 위치한 index를 list의 indexOf 메서드를 통해 찾았다
+ * answer int배열에 max,index를 넣어준 후 return
+ */
 class Solution {
     public int[] solution(int[] array) {
         List<Integer> list = new ArrayList<>();
