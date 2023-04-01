675f537f151997fff2e5cf4797ad5851d5989120
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-04-01 20:58:30 +0900

    [Silver III] Title: 바이러스, Time: 236 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static int[][] network;
+    static boolean[] visited;
+    static int count = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int computerCount = sc.nextInt();
+        int connection = sc.nextInt();
+
+        network = new int[computerCount + 1][computerCount + 1];
+        for (int i = 0; i < connection; i++) {
+            int startNode = sc.nextInt();
+            int endNode = sc.nextInt();
+            network[startNode][endNode] = 1;
+            network[endNode][startNode] = 1;
+        }
+
+        visited = new boolean[computerCount + 1];
+
+        dfs(1);
+        System.out.println(count);
+    }
+
+    private static void dfs(int startNode) {
+        visited[startNode] = true;
+        for (int i = 0; i < network.length; i++) {
+            if (network[startNode][i] ==1 && !visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+    }
+}
