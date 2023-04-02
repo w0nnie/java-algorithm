826ae3c69616becb6d8498ecdf43a78db98772d6
jinwon <kjw55523@gmail.com>
2023-04-02 22:30:00 +0900

    BOJ24479_알고리즘수업_깊이우선탐색1

--- src/main/java/DFS_BFS/BOJ24479_알고리즘수업_깊이우선탐색1.java
+++ src/main/java/DFS_BFS/BOJ24479_알고리즘수업_깊이우선탐색1.java
@@ -1,41 +1,55 @@
 package dfs_bfs;
 
 
-import java.util.Scanner;
+import java.util.*;
 
 public class BOJ24479_알고리즘수업_깊이우선탐색1 {
-    static int n;
-    static int m;
-    static int[] answer;
     static boolean[] visited;
-    static int[][] arr;
+    static List<Integer>[] arr;
+    static int[] answer;
+    static int count = 1;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int startNode = sc.nextInt();
-        answer = new int[n];
 
-        arr = new int[n+1][n+1];
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int startNode = sc.nextInt();
+
+        visited = new boolean[n + 1];
+        answer = new int[n + 1];
+        arr = new ArrayList[n + 1];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
         for (int i = 0; i < m; i++) {
             int stNode = sc.nextInt();
             int endNode = sc.nextInt();
-            arr[stNode][endNode] = 1;
-            arr[endNode][stNode] = 1;
+            arr[stNode].add(endNode);
+            arr[endNode].add(stNode);
         }
-         visited = new boolean[n + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            Collections.sort(arr[i]);
+        }
+
         dfs(startNode);
-        for (int i : answer) {
-            System.out.println(i);
+        for (int i = 1; i < answer.length; i++) {
+            System.out.println(answer[i]);
         }
     }
 
     private static void dfs(int startNode) {
         visited[startNode] = true;
-        answer[startNode - 1] = startNode;
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[startNode][j] == 1 && !visited[j]) {
-                dfs(j);
+        answer[startNode] = count++;
+
+        for (int i = 0; i < arr[startNode].size(); i++) {
+            int nextNode = arr[startNode].get(i);
+            if (!visited[nextNode]) {
+                dfs(nextNode);
             }
         }
     }
