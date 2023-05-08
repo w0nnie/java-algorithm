4791f27b0997c18ab9a66a55c57fec5412ffd3d2
w0nnie <kjw55523@gmail.com>
2023-05-08 22:49:11 +0900

    P12943

--- src/main/java/프로그래머스/level1/P12912/Main.java
+++ src/main/java/프로그래머스/level1/P12912/Main.java
@@ -2,27 +2,27 @@ package 프로그래머스.level1.P12912;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(3, 5));
+        System.out.println(new Solution().solution(626331));
     }
 }
 
 
 class Solution {
-    public long solution(int a, int b) {
-        long answer = 0;
-        if (a == b) {
-            return a;
-        }
-        else if (a < b) {
-            for (int i = a; i <= b; i++) {
-                answer += i;
+    static int answer = 0;
+    public int solution(long num) {
+        while (num != 1) {
+            if (answer == 500) {
+                return -1;
+            }else{
+                if (num % 2 == 0) {
+                    num /= 2;
+                }else{
+                    num = num * 3 + 1;
+                }
+                answer++;
             }
-        } else if (b < a) {
-            for (int i = a; i <= b; i++) {
-                answer += i;
-            }
         }
-        return answer;
+        return answer; // count
     }
 }
 
