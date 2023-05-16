d9fb3412f50da39da6d84dada4e33f0cfbd5063d
w0nnie <kjw55523@gmail.com>
2023-05-16 09:58:19 +0900

    BOJ15651_N과M3

---
+++ src/main/java/DFS_백트래킹/BOJ15651_N과M3.java
@@ -0,0 +1,46 @@
+package DFS_백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15651_N과M3 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+
+        }
+    }
+}
+
