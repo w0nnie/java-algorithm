4ecf5d10521a8a413bc70b3bef3d57a59fa39f54
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-05-19 17:37:26 +0900

    [level 2] Title: 올바른 괄호, Time: 21.12 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack stack = new Stack();
+
+        if(s.length() % 2 !=0){
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c =='(') {
+                stack.push(c);
+            } else{
+                if(stack.isEmpty()) return false;
+                if (stack.peek().equals('(')) {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
