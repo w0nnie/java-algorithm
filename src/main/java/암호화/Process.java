c6deae623eacf9a9811e90a988d484b4ab659dc6
w0nnie <kjw55523@gmail.com>
2023-07-12 10:55:02 +0900

    암호화 sample

---
+++ src/main/java/암호화/Process.java
@@ -0,0 +1,30 @@
+package 암호화;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Process {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        User user = new User(1L, "wlsdnjs!12");
+        System.out.println(getEncSHA256(user.getPassword()));
+    }
+
+
+    public static String getEncSHA256(String password) throws NoSuchAlgorithmException {
+        StringBuffer sb = new StringBuffer();
+
+        final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(password.getBytes());
+
+        final byte[] msgSt = messageDigest.digest();
+
+        for (byte tempStrByte : msgSt) {
+            final String tmpEncTxt  = Integer.toString((tempStrByte & 0xff) + 0x100, 16).substring(1);
+
+            sb.append(tmpEncTxt);
+        }
+
+        return sb.toString();
+    }
+}
