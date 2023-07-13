c6fb0fc9d6b3ea6bf727b44e5545db720526ed25
w0nnie <87374274+w0nnie@users.noreply.github.com>
2023-07-13 15:19:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int ans = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                ans += absolutes[i];
+            }else{
+                ans -= absolutes[i];
+            }
+        }
+        return ans;
+    }
+}
