e7876bfa45e94b305f289e53169adfb581cce13a
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-01-04 11:24:27 +0900

    [Silver IV] Title: 스택, Time: 396 ms, Memory: 18848 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,51 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    /**
+     * push X: 정수 X를 스택에 넣는 연산이다.
+     * pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+     * size: 스택에 들어있는 정수의 개수를 출력한다.
+     * empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+     * top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    */
+    public static List<Integer> stack = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
 
-            if (command.equals("push")) {
-                stack.push(Integer.parseInt(st.nextToken()));
-            }
+        int howManyTime = Integer.parseInt(br.readLine());
 
-            if(command.equals("pop")){
-                if(stack.size() == 0){
-                    System.out.println(-1);
-                }else{
-                    System.out.println(stack.pop());
-                }
-            }
+        for (int i = 0; i < howManyTime; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String work = st.nextToken();
 
-            if (command.equals("size")) {
-                System.out.println(stack.size());
-            }
-
-            if (command.equals("empty")) {
-                if (stack.isEmpty()) {
-                    System.out.println(1);
-                }else{
-                    System.out.println(0);
-                }
-            }
-
-            if (command.equals("top")) {
-                if (stack.size() == 0) {
-                    System.out.println(-1);
-                }else{
-                    System.out.println(stack.peek());
-                }
+            switch (work) {
+                case "push":
+                    int pushNum = Integer.parseInt(st.nextToken());
+                    push(pushNum);
+                    break;
+                case "pop":
+                    System.out.println(pop());
+                    break;
+                case "size":
+                    System.out.println(size());
+                    break;
+                case "empty":
+                    System.out.println(empty());
+                    break;
+                case "top":
+                    System.out.println(top());
+                    break;
             }
         }
+
+        br.close();
+    }
+
+    static void push(int x) {
+        stack.add(x);
+    }
+
+    static int pop() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    static int size() {
+
+        return stack.size();
+    }
+
+    static int empty() {
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+    static int top() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.get(stack.size() - 1);
     }
 }
-
