14a2af676d589e1a3619a95119ad40910b50826c
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-01-10 11:40:11 +0900

    [Silver II] Title: 스택 수열, Time: 360 ms, Memory: 28228 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,46 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.Buffer;
 import java.util.Stack;
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            int temp = Integer.parseInt(br.readLine());
-            arr[i] = temp;
-        }
-        int num = 1;
-        boolean flag = true;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int index = arr[i];
-            if (index >= num) {
-                while (index >= num) {
-                    stack.push(num);
-                    num++;
-                    sb.append("+ \n");
+
+        Stack<Integer> stack = new Stack();
+        int howManyTime = Integer.parseInt(br.readLine());
+        int currentlyPushNum = 0;
+        for (int i = 0; i < howManyTime; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (currentlyPushNum == 0) {
+                currentlyPushNum = num;
+                for (int j = 1; j <= num; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
                 }
                 stack.pop();
-                sb.append("- \n");
-            }else{
-                int a = stack.pop();
-                if (a > index) {
-                    System.out.println("NO");
-                    flag = false;
-                    break;
+                sb.append("-");
+            } else {
+                if (currentlyPushNum < num) {
+                    for (int j = currentlyPushNum + 1; j <= num; j++) {
+                        stack.push(j);
+                        sb.append("\n+");
+                    }
+                    stack.pop();
+                    sb.append("\n-");
+                    currentlyPushNum = num;
                 }else{
-                    sb.append("- \n");
+                    if (stack.peek().equals(num)) {
+                        stack.pop();
+                        sb.append("\n-");
+                    }
                 }
             }
         }
-        if(flag){
+        if (stack.isEmpty()) {
             System.out.println(sb);
+        }else{
+            System.out.print("NO");
         }
+        br.close();
     }
 }
+
