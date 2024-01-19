611e9ccb806b707f10cebe31b786ea97cba03645
w0nnie <kjw55523@gmail.com>
2024-01-19 09:21:32 +0900

    BOJ1406 에디터 풀이

---
+++ src/main/java/자료구조/BOJ1406_에디터.java
@@ -0,0 +1,58 @@
+package 자료구조;
+
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ1406_에디터 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<String> leftStack = new Stack<>();
+        Stack<String> rightStack = new Stack<>();
+        String eng = br.readLine();
+
+        for (int i = 0; i < eng.length(); i++) {
+            char a = eng.charAt(i);
+            leftStack.push(String.valueOf(a));
+        }
+        int howManyTime = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < howManyTime; i++) {
+            String method = br.readLine();
+            char a = method.charAt(0);
+            switch (a) {
+                case 'L':
+                    if (!leftStack.isEmpty()) {
+                        rightStack.push(leftStack.pop());
+                    }
+                    break;
+                case 'D':
+                    if (!rightStack.isEmpty()) {
+                        leftStack.push(rightStack.pop());
+                    }
+                    break;
+                case 'B':
+                    if (!leftStack.isEmpty()) {
+                        leftStack.pop();
+                    }
+                    break;
+                case 'P':
+                    char t = method.charAt(2);
+                    leftStack.push(String.valueOf(t));
+                    break;
+            }
+        }
+        while (!rightStack.isEmpty()) {
+            leftStack.push(rightStack.pop());
+        }
+
+        for (String element : leftStack) {
+            sb.append(element);
+        }
+
+        System.out.println(sb);
+    }
+
+}
