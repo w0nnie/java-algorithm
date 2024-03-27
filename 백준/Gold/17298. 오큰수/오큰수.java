970f30262ec49ef4a951add2bdbc5b8a8fc4028d
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-03-27 22:09:07 +0900

    [Gold IV] Title: 오큰수, Time: 1124 ms, Memory: 172280 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int howManyTimes = Integer.parseInt(br.readLine());
+        int[] arr = new int[howManyTimes];
+        Stack<Integer> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < howManyTimes; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        stack.push(0);
+        for (int i = 1; i < howManyTimes; i++) {
+
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int a : arr) {
+            sb.append(a + " ");
+        }
+        System.out.println(sb.toString());
+    }
+}
