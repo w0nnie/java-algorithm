7aaeb830e7087b486d416076b71b92d329bfabf5
w0nnie <kjw55523@gmail.com>
2024-04-08 17:18:41 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 14616 KB -BaekjoonHub

--- src/main/java/자료구조/BOJ1546_평균구하기.java
+++ src/main/java/자료구조/BOJ1546_평균구하기.java
@@ -1,27 +1,24 @@
 package 자료구조;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class BOJ1546_평균구하기 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt(); // 3
-        int a[] = new int[n]; // n만큼의 사이즈 == 3
-
-        for(int i =0; i< n; i++){
-            a[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int howManyScore = Integer.parseInt(br.readLine());
+        int[] scoreArr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        double maxScore = Arrays.stream(scoreArr).max().getAsInt();
+        double answer = 0;
+        for (double score : scoreArr) {
+            answer += score / maxScore * 100;
         }
-        int max = 0;
-        int sum = 0;
 
-        for(int i =0; i<a.length; i++){
-            if( max < a[i]){
-                max = a[i];
-            }
-            sum += a[i];
-        }
-        System.out.println(sum*100.0/max/n);
+
+        System.out.println(answer / howManyScore);
+
     }
-
 }
