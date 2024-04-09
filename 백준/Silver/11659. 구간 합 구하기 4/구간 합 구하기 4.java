36aba36e8573d9fa39c81aff264db0f2a758d827
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-04-09 09:38:56 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1748 ms, Memory: 59744 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,52 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int elementCount = Integer.parseInt(stk.nextToken());
+        int howManyTime = Integer.parseInt(stk.nextToken());
+        int[] sumArr = new int[elementCount + 1];
 
-        int suNo = Integer.parseInt(st.nextToken()); // 데이터의 개수
-        int quNo = Integer.parseInt(st.nextToken()); // 퀴즈 개수
-        int[] sumArr = new int[suNo + 1]; //구간합배열 1번인덱스 부터
-        
-        sumArrAdd(br, suNo, sumArr);
-        solution(br, quNo, sumArr);
-    }
-
-    /**
-     * start, end 값 입력과 동시에  
-     * sumArr배열에 구간합 공식을 적용하여 원하는값을 도출
-     * @param br
-     * @param quNo
-     * @param sumArr
-     * @throws IOException
-     */
-    private static void solution(BufferedReader br, int quNo, int[] sumArr) throws IOException {
-        StringTokenizer st;
-        for(int q = 0; q< quNo; q++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            System.out.println(sumArr[end] - sumArr[start-1]);
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= elementCount; i++) {
+            sumArr[i] = sumArr[i - 1] + Integer.parseInt(stk.nextToken());
         }
-    }
 
-    /**
-     * sumArr 누적
-     * @param br
-     * @param suNo
-     * @param sumArr
-     * @throws IOException
-     */
-    private static void sumArrAdd(BufferedReader br, int suNo, int[] sumArr) throws IOException {
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i<= suNo; i++){
-            sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
+        for (int i = 0; i < howManyTime; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+
+            System.out.println(sumArr[b] - sumArr[a-1]);
         }
     }
 }
