6fd1692d67539f2ee19167d6fbb594cc7cca6ac3
w0nnie <kjw55523@gmail.com>
2024-04-09 09:41:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1748 ms, Memory: 59744 KB -BaekjoonHub

--- src/main/java/자료구조/BOJ11659_구간합구하기.java
+++ src/main/java/자료구조/BOJ11659_구간합구하기.java
@@ -1,28 +1,27 @@
 package 자료구조;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ11659_구간합구하기 {
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int suNo = Integer.parseInt(st.nextToken()); // 데이터의 개수
-        int quNo = Integer.parseInt(st.nextToken()); // 퀴즈 개수
-        long[] s = new long[suNo + 1];
-        st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=suNo; i++){
-            s[i] = s[i-1] + Integer.parseInt(st.nextToken());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int elementCount = Integer.parseInt(stk.nextToken());
+        int howManyTime = Integer.parseInt(stk.nextToken());
+        int[] sumArr = new int[elementCount + 1];
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= elementCount; i++) {
+            sumArr[i] = sumArr[i - 1] + Integer.parseInt(stk.nextToken());
         }
-        for(int q=0; q<quNo; q++){
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            System.out.println(s[j] - s[i-1]);
+
+        for (int i = 0; i < howManyTime; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+
+            System.out.println(sumArr[b] - sumArr[a-1]);
         }
     }
-
 }
