7938f4fb6d8c07b075b33697c7bf912891c77a6a
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-04-18 23:53:30 +0900

    [Bronze I] Title: 최소공배수, Time: 188 ms, Memory: 15080 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            int originA = a;
+            int originB = b;
+
+            if (a < b) {
+                b = a;
+                a = originB;
+
+            }
+            while (b != 0) {
+                int r = a % b;
+
+                a = b;
+                b = r;
+            }
+
+            System.out.println(originA * originB / a);
+        }
+    }
+}
