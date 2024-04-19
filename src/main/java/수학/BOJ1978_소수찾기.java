b5fc350ac6b5da3d7c2d06fd88daec429e213cc1
w0nnie <kjw55523@gmail.com>
2024-04-19 10:56:01 +0900

    [Bronze II] Title: 소수 찾기, Time: 128 ms, Memory: 14436 KB -BaekjoonHub

---
+++ src/main/java/수학/BOJ1978_소수찾기.java
@@ -0,0 +1,32 @@
+package 수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class BOJ1978_소수찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] a = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int count = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] != 1) count += getPrimeNumber(a[i]);
+        }
+        System.out.println(count);
+    }
+
+    private static int getPrimeNumber(int i) {
+
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
+
