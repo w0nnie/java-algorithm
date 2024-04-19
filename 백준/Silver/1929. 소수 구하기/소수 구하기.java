26086d293de1ab11fd13a3b9e6fccaf3cacf090c
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-04-19 15:58:35 +0900

    [Silver III] Title: 소수 구하기, Time: 328 ms, Memory: 24080 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(stk.nextToken());
+        int N = Integer.parseInt(stk.nextToken());
+        // M부터 N사이의 소수들을 출력하여라
+        // M == 3일때 2는 출력되면 안된다
+
+        boolean[] numFlag = new boolean[N + 1];
+
+        numFlag[0] = true;
+        numFlag[1] = true;
+
+        // N == 50
+        // 2 ~ 7
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            for (int j = i + i; j <= N; j+=i) {
+                numFlag[j] = true;
+            }
+        }
+
+        for (int i = M; i < numFlag.length; i++) {
+            if (!numFlag[i]) {
+                sb.append(i + "\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
