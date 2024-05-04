c4bddb3e582ba0f7f1f7536f07309122d1a6d34c
jinwon Kwon <kjw55523@gmail.com>
2024-05-04 17:08:55 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1672 ms, Memory: 122068 KB -BaekjoonHub

---
+++ src/main/java/누적합/BOJ11660_구간합구하기5.java
@@ -0,0 +1,43 @@
+package 누적합;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ11660_구간합구하기5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int[][] arr = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            stk = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(stk.nextToken()) + arr[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            int c = Integer.parseInt(stk.nextToken());
+            int d = Integer.parseInt(stk.nextToken());
+            int ans = 0;
+            for (int j = a; j <= c; j++) {
+                ans += arr[j][d];
+            }
+            for (int j = a; j <= c; j++) {
+                ans -= arr[j][b - 1];
+            }
+
+            bw.write(ans + "\n");
+        }
+
+        bw.flush();
+    }
+}
