647ee69604d1270ac3f641deeeed40f24526f235
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-05-08 21:13:27 +0900

    [Gold III] Title: 나머지 합, Time: 512 ms, Memory: 120596 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int num = Integer.parseInt(stk.nextToken());
+
+        stk = new StringTokenizer(br.readLine());
+        long[] arr = new long[n];
+        long[] remain = new long[num];
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                arr[i] = Integer.parseInt(stk.nextToken());
+            } else {
+                arr[i] += arr[i - 1] + Integer.parseInt(stk.nextToken());
+            }
+            int remainNum = (int) (arr[i] % num);
+            if (remainNum == 0) {
+                ans++;
+            }
+
+            remain[remainNum]++;
+        }
+        for (int i = 0; i < remain.length; i++) {
+
+            ans += remain[i] * (remain[i] - 1) / 2;
+        }
+
+        System.out.print(ans);
+    }
+}
+
+
+
