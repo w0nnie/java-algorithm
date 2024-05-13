5abfd993aec69fc302f288232bd62df56cd17e66
jinwon Kwon <kjw55523@gmail.com>
2024-05-13 22:39:10 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18220 KB -BaekjoonHub

---
+++ src/main/java/DP/BOJ1463_1로만들기.java
@@ -0,0 +1,27 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1463_1로만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+            }
+        }
+
+        System.out.println(arr[n]);
+    }
+}
