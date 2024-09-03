08d75ed6c26d0a6f809618024238767432ce8d22
jinwon Kwon <kjw55523@gmail.com>
2024-09-03 23:26:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 164 ms, Memory: 17760 KB -BaekjoonHub

---
+++ src/main/java/그리디/BOJ2839_설탕배달.java
@@ -0,0 +1,38 @@
+package 그리디;
+
+import java.util.Scanner;
+
+public class BOJ2839_설탕배달 {
+    private static final int THREE_KG = 3;
+    private static final int FIVE_KG = 5;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(getAnswer(num));
+    }
+
+    private static int getAnswer(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            if (num % FIVE_KG == 0) {
+                count += num / FIVE_KG;
+                num = 0;
+                break;
+            } else {
+                num -= THREE_KG;
+                count++;
+            }
+        }
+
+        if (num != 0) {
+            return -1;
+        }
+
+        return count;
+    }
+
+}
