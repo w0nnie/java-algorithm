d28181affffc97ff2c80662ad3d01ba77e9a17fe
w0nnie <kjw55523@gmail.com>
2024-09-03 11:20:21 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 172 ms, Memory: 18060 KB -BaekjoonHub

---
+++ src/main/java/수학/BOJ_1676_팩토리얼0의개수.java
@@ -0,0 +1,40 @@
+package 수학;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_1676_팩토리얼0의개수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        BigInteger factorial = getFactorial(num);
+        int zeroCount = getZeroCount(factorial);
+
+        System.out.println(zeroCount);
+    }
+
+    private static int getZeroCount(BigInteger factorial) {
+        int zeroCount = 0;
+        String fatorialString = String.valueOf(factorial);
+        for (int i = fatorialString.length() - 1; i >= 0; i--) {
+            char temp = fatorialString.charAt(i);
+            if (temp == '0') {
+                zeroCount++;
+            }else{
+                break;
+            }
+        }
+        return zeroCount;
+    }
+
+    private static BigInteger getFactorial(int num) {
+        BigInteger factorial = new BigInteger("1");
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        return factorial;
+    }
+}
