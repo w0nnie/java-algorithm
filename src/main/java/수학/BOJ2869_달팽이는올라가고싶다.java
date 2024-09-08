63dfc91096441e8fbcfd426f70472c42727acef1
jinwon Kwon <kjw55523@gmail.com>
2024-09-08 14:40:57 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ src/main/java/수학/BOJ2869_달팽이는올라가고싶다.java
@@ -0,0 +1,23 @@
+package 수학;
+
+import java.util.Scanner;
+
+public class BOJ2869_달팽이는올라가고싶다 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int up = sc.nextInt();
+        int down = sc.nextInt();
+        int height = sc.nextInt();
+        int count;
+        int temp = (height - up) % (up - down);
+
+        if (temp == 0) {
+            count = (height - up) / (up - down) + 1;
+        } else {
+            count = (height - up) / (up - down) + 2;
+        }
+
+        System.out.println(count);
+    }
+}
