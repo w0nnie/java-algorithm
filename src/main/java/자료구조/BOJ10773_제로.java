0f31b734c2461151f20f565a87ddb4adccb7d65d
jinwon Kwon <kjw55523@gmail.com>
2024-09-17 21:52:02 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 24172 KB -BaekjoonHub

---
+++ src/main/java/자료구조/BOJ10773_제로.java
@@ -0,0 +1,32 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ10773_제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(num);
+            }
+        }
+
+        for (Integer integer : stack) {
+            answer += integer;
+        }
+        System.out.println(answer);
+    }
+}
