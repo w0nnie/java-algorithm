a715cfa0a8e8f1a3d78b56fba680a93097abc3ea
w0nnie <87374274+w0nnie@users.noreply.github.com>
2024-09-19 09:37:37 +0900

    [Silver III] Title: 프린터 큐, Time: 152 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,60 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk;
 
-        int howManytimes = Integer.parseInt(br.readLine());
-        Queue<int []> queue;
+        int testCase = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < howManytimes; i++) {
+        for (int i = 0; i < testCase; i++) {
             stk = new StringTokenizer(br.readLine());
-            queue = new LinkedList<>();
-            int N = Integer.parseInt(stk.nextToken());
-            int position = Integer.parseInt(stk.nextToken());
+            int documents = Integer.parseInt(stk.nextToken()); //문서 수
+            int location = Integer.parseInt(stk.nextToken());  // 찾고자 하는 문서의 위치
 
             stk = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < N; j++) {
-                queue.add(new int[] {j, Integer.parseInt(stk.nextToken())});
+            Queue<int[]> queue = new LinkedList<>(); // 중요도 배열
+            int index = 0;
+            while (stk.hasMoreTokens()) {
+                queue.add(new int[]{index, Integer.parseInt(stk.nextToken())});
+                index++;
             }
-            System.out.println(method(queue, position));
-        }
 
-    }
+            int printCount = 1;
 
-private static int method(Queue<int []> queue, int position) {
-        int count = 0;
+            while (true) {
+                int max = Integer.MIN_VALUE;
+                for (int[] ints : queue) { // queue의 원소중 max값을 찾는 로직
+                    int important = ints[1];
+                    if (important > max) {
+                        max = important;
+                    }
+                }
 
-        while (true) {
-            int[] front = queue.poll();
-            boolean isMax = true; // front 원소가 가장 큰 원소인지 판단하는 변수
+                if (queue.peek()[1] == max) {
+                    int[] a = queue.poll();
 
-            for (int[] curQ : queue) {
-                if (front[1] < curQ[1]) {
-                    isMax = false;
-                    break;
+                    if (a[0] == location) { 
+                        break;
+                    }
+                    printCount++;
+                } else {
+                    queue.add(queue.poll());
                 }
             }
-
-            if (isMax) {
-                count++;
-                if (front[0] == position) {
-                    break;
-                }
-            }else{
-                queue.add(front);
-            }
-
+            System.out.println(printCount);
         }
-        return count;
     }
 }
