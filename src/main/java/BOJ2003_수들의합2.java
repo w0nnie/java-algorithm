29cc0785c29284f3dc45ccde28e85446ae88dd57
w0nnie <kjw55523@gmail.com>
2024-10-04 16:10:49 +0900

    [Silver II] Title: 과일 탕후루, Time: 416 ms, Memory: 37340 KB -BaekjoonHub

---
+++ src/main/java/BOJ2003_수들의합2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2003_수들의합2 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
+        int[] hap = new int[N + 1];
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            hap[i] += hap[i - 1] + Integer.parseInt(stk.nextToken());
+        }
+        int start = 1;
+        int end = 1;
+        int count = 0;
+        while (start <= N) {
+            int rangeHap = hap[end] - hap[start - 1];
+
+            if (rangeHap == M) {
+                count++;
+            }
+            if (rangeHap <= M && end < N) {
+                end++;
+            } else {
+                start++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/BOJ30804_과일탕후루.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ30804_과일탕후루 {
+
+    static int[] list;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int answer = getAnswer(N);
+        System.out.println(answer);
+    }
+
+    static int getAnswer(int N) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int start = 0;
+        int max = 0;
+
+        for (int end = 0; end < N; end++) {
+            countMap.put(list[end], countMap.getOrDefault(list[end], 0) + 1);
+
+            while (countMap.size() > 2) { //과일이 2종류를 넘게되면
+                countMap.put(list[start], countMap.get(list[start]) - 1);
+                if (countMap.get(list[start]) == 0) {
+                    countMap.remove(list[start]);
+                }
+                start++;
+            }
+
+            max = Math.max(max, end - start + 1);
+        }
+
+        return max;
+    }
+}
