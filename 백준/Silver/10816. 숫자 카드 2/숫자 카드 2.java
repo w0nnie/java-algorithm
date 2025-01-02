845db27a640abe68900f33fa92c580fdba67eebc
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-02 16:00:55 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1048 ms, Memory: 183480 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,57 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    public static void main(String[]args) throws IOException{
+    public static void main(String[] args) throws IOException{
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        //상근이 보유중인 카드 개수
-        int hasCardNum = Integer.parseInt(stringTokenizer.nextToken());
-        //상근이 보유중인 숫자카드
-        int hasCards[] = new int[hasCardNum];
-
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        //상근이 보유중인 숫자카드 입력
-        for (int i = 0; i < hasCardNum; i++) {
-            hasCards[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-        //질의 수
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int question = Integer.parseInt(stringTokenizer.nextToken());
-        //key값에 적힌 숫자가 상근이 몇개가지고있는지 value에 들어간다.
-        Map<Integer, Integer> questionCards = new HashMap<>();
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        List<Integer> cardArr = new ArrayList<>();
-        for (int i = 0; i < question; i++) {
-            int nextCardNum = Integer.parseInt(stringTokenizer.nextToken());
-            questionCards.put(nextCardNum, 0);
-            cardArr.add(nextCardNum);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(stk.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (int i = 0; i < hasCards.length; i++) {
-            if(questionCards.containsKey(hasCards[i])){
-                int count = questionCards.get(hasCards[i]) + 1;
-                questionCards.put(hasCards[i], count);
-            }
+        int m = Integer.parseInt(br.readLine());
+        stk = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i< m; i++){
+            int findNum = Integer.parseInt(stk.nextToken());
+            sb.append(map.getOrDefault(findNum, 0) + " ");
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < cardArr.size(); i++) {
-            int card = questionCards.get(cardArr.get(i));
-            if (i == cardArr.size()-1) {
-                stringBuilder.append(card);
-            }else{
-                stringBuilder.append(card + " ");
-            }
-        }
-        bufferedWriter.write(String.valueOf(stringBuilder));
-        bufferedWriter.flush();
-        bufferedWriter.close();
-        bufferedReader.close();
+
+        System.out.print(sb);
     }
-
 }
