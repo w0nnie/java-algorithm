fef87e3ff85a60726debabf5fc2a8692bf644dc2
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-06 17:53:52 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 312 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = br.readLine();
+
+            if (line.equals(".")) {
+                break;
+            }
+            Stack<String> stack = new Stack<>();
+
+            for (int i = 0; i < line.length(); i++) {
+                String a = String.valueOf(line.charAt(i));
+                // 종료조건 .
+                if (a.equals(".")) {
+                    if (stack.size() == 0) {
+                        System.out.println("yes");
+                    } else {
+                        System.out.println("no");
+                    }
+                    break;
+                    
+                } else if (a.equals("(") || a.equals("[")) {
+                    stack.add(a);
+                    
+                } else if (a.equals(")")) {
+                    if (stack.isEmpty()) {
+                        System.out.println("no");
+                        break;
+                    }
+
+                    String shape = stack.peek();
+
+                    if (shape.equals("(")) {
+                        stack.pop();
+                    } else {
+                        stack.add(a);
+                    }
+                } else if (a.equals("]")) {
+                    if (stack.isEmpty()) {
+                        System.out.println("no");
+                        break;
+                    }
+
+                    String shape = stack.peek();
+
+                    if (shape.equals("[")) {
+                        stack.pop();
+                    } else {
+                        stack.add(a);
+                    }
+                }
+            }
+        }
+    }
+}
