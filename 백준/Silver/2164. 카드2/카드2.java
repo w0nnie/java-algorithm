a0cc3d47e56a8951e3759022bb59ed06fd9dabfa
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-06 21:45:08 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 53380 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,32 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
+        Queue q = new LinkedList<>();
 
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            q.add(i);
         }
-        boolean flag = true;
 
-        while (queue.size() > 1) {
-
-            if (flag) {
-                queue.remove(queue.peek());
-                flag = false;
-            } else {
-                queue.add(queue.peek());
-                queue.remove(queue.peek());
-                flag = true;
+        while (true) {
+            // 종료 조건
+            if (q.size() == 1) {
+                System.out.println(q.peek());
+                break;
             }
+            q.poll();
+            int peek = (int) q.poll();
+            q.add(peek);
         }
-
-        System.out.println(queue.peek());
     }
 }
