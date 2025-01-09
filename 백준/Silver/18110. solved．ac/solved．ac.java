ab8a51cd7c47d8eba3c4a5bea58ebeeb0378c085
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-09 15:06:30 +0900

    [Silver IV] Title: solved.ac, Time: 332 ms, Memory: 38616 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        float averageCut = 0.15f;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+
+        int exceptionNum = Math.round(averageCut * n);
+        for (int i = 0; i < exceptionNum; i++) {
+            arr[i] = 0;
+        }
+
+        for (int i = arr.length - 1; i > arr.length - 1 - exceptionNum; i--) {
+            arr[i] = 0;
+        }
+
+        int hap = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            hap += arr[i];
+            if (arr[i] == 0) {
+                zeroCount++;
+            }
+        }
+
+        int answer = Math.round(hap / (float)(arr.length - zeroCount));
+
+        System.out.println(answer);
+    }
+}
