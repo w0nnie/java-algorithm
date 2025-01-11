68e304f5a7c3f8830a85f78053cc454339f43e56
jinwon Kwon <kjw55523@gmail.com>
2025-01-11 20:05:38 +0900

    solved.ac class3 풀이

---
+++ src/main/java/solvedac_class3/BOJ1620_나는야포켓몬마스터이다솜.java
@@ -0,0 +1,41 @@
+package solvedac_class3;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1620_나는야포켓몬마스터이다솜 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int monCount = Integer.parseInt(stk.nextToken());
+        int questionCount = Integer.parseInt(stk.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= monCount; i++) {
+            String pokeMon = br.readLine();
+            map.put(pokeMon, i);
+            list.add(pokeMon);
+        }
+
+        for (int i = 0; i < questionCount; i++) {
+            String input = br.readLine();
+            boolean isNum = true;
+            int pokeMonNum = 0;
+            try {
+                pokeMonNum = Integer.parseInt(input);
+            } catch (NumberFormatException  e) {
+                isNum = false;
+            }
+
+            if (isNum) {
+                System.out.println(list.get(pokeMonNum - 1));
+            } else {
+                System.out.println(map.get(input));
+            }
+        }
+
+    }
+}
