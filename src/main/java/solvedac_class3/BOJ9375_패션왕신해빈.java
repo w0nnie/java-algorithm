0cb2811ccaf922939e14132847edd75af6dcce7d
w0nnie <kjw55523@gmail.com>
2025-01-15 08:39:23 +0900

    solved.ac 풀이

---
+++ src/main/java/solvedac_class3/BOJ9375_패션왕신해빈.java
@@ -0,0 +1,34 @@
+package solvedac_class3;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ9375_패션왕신해빈 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < testCase; i++){
+            int n = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                stk = new StringTokenizer(br.readLine());
+                String key = stk.nextToken();
+                String value = stk.nextToken();
+
+                map.put(value, map.getOrDefault(value, 0) + 1);
+            }
+
+            int answer = 1;
+            for (String s : map.keySet()) {
+                answer *= map.get(s) + 1;
+            }
+
+            System.out.println(answer - 1);
+        }
+    }
+}
