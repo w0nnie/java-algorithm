a58fba842316a047cf3ac49d2e64ece2f541c7d5
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-15 10:24:30 +0900

    [Silver III] Title: 01타일, Time: 292 ms, Memory: 57488 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static long[] dp;
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        dp = new long[1000001];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        long answer = tile(n);
+
+        System.out.println(answer);
+    }
+
+    public static long tile(int n) {
+        if (dp[n] == -1) {
+            dp[n] = (tile(n - 1) + tile(n - 2)) % 15746;
+        }
+        return dp[n];
+    }
+}
