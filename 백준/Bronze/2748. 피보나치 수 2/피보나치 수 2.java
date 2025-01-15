83511e4905ae79d1260555cffd84d842667fa494
w0nnie <87374274+w0nnie@users.noreply.github.com>
2025-01-15 09:20:37 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static Long[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new Long[n + 1];
+
+        arr[0] = 0L;
+        arr[1] = 1L;
+
+        Long answer = fibo(n);
+        System.out.println(answer);
+    }
+
+
+    public static Long fibo(int n){
+        if (arr[n] == null) {
+            arr[n] = fibo(n - 1) + fibo(n - 2);
+        }
+
+        return arr[n];
+    }
+}
