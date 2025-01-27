38a5a9ef9fdd32dbd6bfedcd6a592d98a035338a
jinwon Kwon <kjw55523@gmail.com>
2025-01-27 12:43:22 +0900

    solved.ac 풀이

---
+++ src/main/java/solvedac_class3/BOJ15651_N과M3.java
@@ -0,0 +1,38 @@
+package solvedac_class3;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ15651_N과M3 {
+    static int n;
+    static int m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        arr = new int[m];
+
+        getDfs(0);
+        System.out.println(sb);
+    }
+
+    public static void getDfs(int depth) {
+
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            getDfs(depth + 1);
+        }
+    }
+}
