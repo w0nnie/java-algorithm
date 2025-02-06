e5358c6579a845b27cd53a5fe15173c46c0d582c
w0nnie <kjw55523@gmail.com>
2025-02-06 16:54:04 +0900

    solved.ac 풀이

---
+++ src/main/java/solvedac_class3/BOJ10974_모든순열.java
@@ -0,0 +1,47 @@
+package solvedac_class3;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10974_모든순열 {
+
+    static int n;
+
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        visit = new boolean[n];
+        sb = new StringBuilder();
+
+        getDfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void getDfs(int depth) {
+
+        if (depth == n) {
+            for (int num : arr) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visit[i - 1]) {
+                visit[i - 1] = true;
+                arr[depth] = i;
+                getDfs(depth + 1);
+                visit[i - 1] = false;
+            }
+        }
+    }
+}
